78de238a805eb438271f9c0c24999a8f9d0e5d4b
vipulhmistry <vipul.harish.mistry@gmail.com>
2025-07-30 20:06:55 +0100

    Section 14 Chapter 130

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.Products.Features.CreateProduct;
+
+public record CreateProductRequest(ProductDto Product);
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResponse>();
+            return Results.Created($"/products/{response.Id}", response);
+        })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product Description");
+    }
+}
